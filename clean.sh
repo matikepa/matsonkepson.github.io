7e78b882456da59621a4eededb127f5b7114f8d7
matikepa <mati@kepa.eu.org>
2025-08-19 15:39:12 +0200

    startup refactor
    Signed-off-by: matikepa <mati@kepa.eu.org>

--- clean.sh
+++ clean.sh
@@ -4,3 +4,9 @@ rm -rf ~/.cache/hugo_cache/
 rm -rf ./public
 rm -rf ./resources
 rm -rf .hugo_build.lock
+echo '>> hugo cleanup done.'
+
+# Run pre-commit hooks cleanup
+pre-commit gc
+pre-commit clean
+echo '>> pre-commit cleanup done.'
