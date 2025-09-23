87bee745afe65ad7349bb1da7f1e19163b6356ad
mati kepa <mati@kepa.eu.org>
2025-09-23 10:42:51 +0200

    reactivate GA
    Signed-off-by: mati kepa <mati@kepa.eu.org>

--- scripts/gtag.js
+++ scripts/gtag.js
@@ -1,23 +1,23 @@
-// function addGoogleAnalyticsTag(trackingId) {
-//   // Load the gtag.js script asynchronously
-//   const script = document.createElement('script');
-//   script.async = true;
-//   script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
-//   document.head.appendChild(script);
+function addGoogleAnalyticsTag(trackingId) {
+  // Load the gtag.js script asynchronously
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
+  document.head.appendChild(script);
 
-//   // Initialize the dataLayer and gtag function
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag() {
-//     dataLayer.push(arguments);
-//   }
+  // Initialize the dataLayer and gtag function
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
 
-//   // Initialize gtag
-//   gtag('js', new Date());
-//   gtag('config', trackingId);
-// }
+  // Initialize gtag
+  gtag('js', new Date());
+  gtag('config', trackingId);
+}
 
-// // Call the function with your tracking ID
-// addGoogleAnalyticsTag('G-81E77KGMGF');
+// Call the function with your tracking ID
+addGoogleAnalyticsTag('G-81E77KGMGF');
 
 /**
  * Enhanced Cookie Consent Manager
